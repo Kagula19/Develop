c17ae5dfc1177063d66818e612ce16109a2e9b3f
Kagula19 <baciu.gabriel09102006@gmail.com>
2025-10-28 15:41:04 +0100

    Polimorfismo

---
+++ EsPolimorfismo/src/Main.java
@@ -0,0 +1,7 @@
+
+void main() {
+    Forma forma = new Forma();
+    Rettangolo rettangolo = new Rettangolo(10.5,5);
+    Triangolo triangolo = new Triangolo(10, 2);
+    triangolo.calcolaArea();
+}
