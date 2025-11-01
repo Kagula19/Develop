39ba9d2b3a897d18c2a404442a7d0b37fffbfaf8
Kagula19 <baciu.gabriel09102006@gmail.com>
2025-11-01 22:07:52 +0100

    Date2

---
+++ EsDate2/src/Main.java
@@ -0,0 +1,14 @@
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class Main{
+    public static void main(String[] args){
+        OffsetDateTime offsetData = OffsetDateTime.parse("2002-03-01T13:00:00Z");
+        ZonedDateTime data = offsetData.atZoneSimilarLocal(ZoneId.of("Europe/Rome"));
+        System.out.println("data 1 " + data.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL)));
+
+    }
+}
